c55739eaf6a6fbef8a810f014db86b0883c8df2f
Ariel kaszycki <arielkasz35@gmail.com>
2025-08-23 17:40:29 -0500

    initiate files

---
+++ index.js
@@ -0,0 +1,34 @@
+let scoreHome = document.getElementById("score-home")
+console.log(scoreHome)
+let scoreGuest = document.getElementById("score-guest")
+console.log(scoreGuest)
+let score = 0
+
+function victoryHome(){
+    score += 1
+    scoreHome.textContent = score
+}
+
+function defeatHome(){
+    score -= 1
+    scoreHome.textContent = score
+}
+
+function divaHome(){
+    score += 2
+    scoreHome.textContent = score
+}
+function victoryAway(){
+    score += 1
+    scoreGuest.textContent = score
+}
+
+function defeatAway(){
+    score -= 1
+    scoreGuest.textContent = score
+}
+
+function divaAway(){
+    score += 2
+    scoreGuest.textContent = score
+}
